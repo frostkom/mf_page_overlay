32ca33bb679d81d87cc3933641cf14f2093f9c13
frostkom <martin.fors@frostkom.se>
2024-02-27 17:38:13 +0100

    Add icon to remove acceptance

--- include/classes.php
+++ include/classes.php
@@ -49,23 +49,62 @@ class mf_page_overlay
 		$obj_cron->end();
 	}
 
+	function get_overlay_pages()
+	{
+		global $wpdb;
+
+		$out = array();
+
+		$result = $wpdb->get_results($wpdb->prepare("SELECT meta_value FROM ".$wpdb->posts." INNER JOIN ".$wpdb->postmeta." ON ".$wpdb->posts.".ID = ".$wpdb->postmeta.".post_id WHERE post_type = %s AND meta_key = %s AND meta_value > 0 GROUP BY meta_value", 'page', $this->meta_prefix.'page_id'));
+
+		foreach($result as $r)
+		{
+			if($r->meta_value > 0)
+			{
+				$out[] = $r->meta_value;
+			}
+		}
+
+		return $out;
+	}
+
 	function rwmb_meta_boxes($meta_boxes)
 	{
 		if(IS_EDITOR)
 		{
+			$post_id = check_var('post');
+
+			if($post_id > 0 && in_array($post_id, $this->get_overlay_pages()))
+			{
+				$arr_fields = array(
+					array(
+						'name' => __("Hide when accepted", 'lang_page_overlay'),
+						'id' => $this->meta_prefix.'hide_when_accepted',
+						'type' => 'select',
+						'options' => get_yes_no_for_select(),
+						'std' => 'no',
+					),
+				);
+			}
+
+			else
+			{
+				$arr_fields = array(
+					array(
+						'name' => __("Page", 'lang_page_overlay')." <a href='".admin_url("post-new.php?post_type=page&post_title=".__("Popup", 'lang_page_overlay')."&content=.button_close")."'><i class='fa fa-plus-circle fa-lg'></i></a>",
+						'id' => $this->meta_prefix.'page_id',
+						'type' => 'page',
+					),
+				);
+			}
+
 			$meta_boxes[] = array(
 				'id' => $this->meta_prefix.'settings',
 				'title' => __("Page Overlay", 'lang_page_overlay'),
 				'post_types' => array('page'),
 				'context' => 'side',
 				'priority' => 'low',
-				'fields' => array(
-					array(
-						'name' => __("Page", 'lang_page_overlay')." <a href='".admin_url("post-new.php?post_type=page&post_title=".__("Popup", 'lang_page_overlay')."&content=.button_close")."'><i class='fa fa-plus-circle fa-lg'></i></a>",
-						'id' => $this->meta_prefix.'page_id',
-						'type' => 'page',
-					),
-				)
+				'fields' => $arr_fields,
 			);
 		}
 
@@ -76,26 +115,17 @@ class mf_page_overlay
 	{
 		global $post;
 
-		if(isset($post->ID) && $post->ID > 0)
+		if(isset($post->ID) && $post->ID > 0) // && !is_user_logged_in()
 		{
-			$page_overlay = get_post_meta($post->ID, $this->meta_prefix.'page_id', true);
+			$overlay_page_id = get_post_meta($post->ID, $this->meta_prefix.'page_id', true);
 
-			if($page_overlay > 0)
+			if($overlay_page_id > 0)
 			{
-				$plugin_include_url = plugin_dir_url(__FILE__);
-				$plugin_version = get_plugin_version(__FILE__);
-
-				mf_enqueue_style('style_page_overlay', $plugin_include_url."style.css", $plugin_version);
-				mf_enqueue_script('script_page_overlay', $plugin_include_url."script.js", $plugin_version);
-
-				$this->body_class = "has_page_overlay";
-
-				//$post_content = apply_filters('the_content', mf_get_post_content($page_overlay));
 				ob_start();
 
 				query_posts(array(
 					'post_type' => 'page',
-					'p' => $page_overlay,
+					'p' => $overlay_page_id,
 				));
 
 				while(have_posts())
@@ -109,9 +139,32 @@ class mf_page_overlay
 
 				wp_reset_query();
 
-				$this->footer_output = "<div id='overlay_page' class='overlay_container modal disable_close'>
-					<div>".$post_content."</div>
-				</div>";
+				if($post_content != '')
+				{
+					$overlay_hide_when_accepted = get_post_meta($overlay_page_id, $this->meta_prefix.'hide_when_accepted', true);
+
+					$this->body_class = "has_page_overlay";
+
+					$plugin_include_url = plugin_dir_url(__FILE__);
+					$plugin_version = get_plugin_version(__FILE__);
+
+					mf_enqueue_style('style_page_overlay', $plugin_include_url."style.css", $plugin_version);
+					mf_enqueue_script('script_page_overlay', $plugin_include_url."script.js", $plugin_version);
+
+					$this->footer_output = "<div id='overlay_page' data-overlay_page_id='".$overlay_page_id."' data-overlay_hide_when_accepted='".$overlay_hide_when_accepted."' class='overlay_container modal disable_close'>
+						<div>".$post_content."</div>
+					</div>";
+
+					if(IS_SUPER_ADMIN && $overlay_hide_when_accepted == 'yes')
+					{
+						$this->footer_output .= "<div id='overlay_page_accepted'>
+							<span class='fa-stack fa-2x' title='".__("You have accepted. Do you wish to remove this acceptance?", 'lang_page_overlay')."'>
+								<i class='fas fa-cookie-bite fa-stack-1x'></i>
+								<i class='fas fa-ban fa-stack-2x red'></i>
+							</span>
+						</div>";
+					}
+				}
 			}
 		}
 	}
--- include/script.js
+++ include/script.js
@@ -1,8 +1,67 @@
 jQuery(function($)
 {
-	$(document).on('click', "#overlay_page .button_close, #overlay_page a[href='#']", function(e)
+	var dom_overlay_page = $("#overlay_page"),
+		overlay_page_id = dom_overlay_page.data('overlay_page_id'),
+		overlay_hide_when_accepted = dom_overlay_page.data('overlay_hide_when_accepted');
+
+	if(overlay_hide_when_accepted == 'yes')
 	{
-		$(this).parents("#overlay_page").fadeOut().parents("body").removeClass('has_page_overlay');
+		var dom_overlay_page_accepted = $("#overlay_page_accepted");
+
+		dom_overlay_page.each(function()
+		{
+			if(document.cookie.indexOf("cookie_page_overlay_" + overlay_page_id + "=") !== -1)
+			{
+				dom_overlay_page.fadeOut().parents("body").removeClass('has_page_overlay');
+
+				if(dom_overlay_page_accepted.length > 0)
+				{
+					dom_overlay_page_accepted.fadeIn();
+				}
+			}
+
+			else
+			{
+				dom_overlay_page.fadeIn();
+
+				if(dom_overlay_page_accepted.length > 0)
+				{
+					dom_overlay_page_accepted.fadeOut();
+				}
+			}
+		});
+
+		if(dom_overlay_page_accepted.length > 0)
+		{
+			dom_overlay_page_accepted.on('click', "span", function()
+			{
+				document.cookie = "cookie_page_overlay_" + overlay_page_id + "=true; path=/; max-age=0";
+				document.cookie = "cookie_page_overlay_" + overlay_page_id + "=true; max-age=0";
+
+				dom_overlay_page.fadeIn().parents("body").addClass('has_page_overlay');
+				dom_overlay_page_accepted.fadeOut();
+
+				return false;
+			});
+		}
+	}
+
+	dom_overlay_page.on('click', ".button_close, a[href='#']", function()
+	{
+		dom_overlay_page.fadeOut().parents("body").removeClass('has_page_overlay');
+
+		if(overlay_hide_when_accepted == 'yes')
+		{
+			var d = new Date();
+			d.setTime(d.getTime() + (365 * 24 * 60 * 60 * 1000));
+
+			document.cookie = "cookie_page_overlay_" + overlay_page_id + "=true; path=/; expires=" + d.toUTCString();
+
+			if(dom_overlay_page_accepted.length > 0)
+			{
+				dom_overlay_page_accepted.fadeIn();
+			}
+		}
 
 		return false;
 	});
